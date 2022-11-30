178a84dcf77fe4d0f3eced5f54e4eb927e6e5a4e
Hosamov <hosamov@hotmail.com>
2022-11-30 12:04:47 -0800

    update readme.md, add all expected routes

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # exclusions-db
 An exclusions database that is web-based using Node, express and MongoDB/Mongoose
 
-
 # Routes
 ## GET Routes:
 - / - root, redirects to /login or /home, depending on authentication status
 - /login - renders the login page
+- /register - renders the register page, accessible only by Admin for adding new users.
 - /home - renders the exclusions page, displaying all ACTIVE exclusion orders
   - Displays all exclusions, which will be clickable to expand basic data.
     Further clickable to display the whole exclusion order (for print)
--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/static', express.static('public'));
 
+
+//*********** GET ROUTES ************/
 // Home GET route - redirects to login or homepage depending on authorization
 app.get('/', (req, res, next) => {
   res.send('Root page');
@@ -26,10 +28,69 @@ app.get('/login', (req, res, next) => {
   res.send('Login page');
 });
 
+app.get('/register_user', (req, res, next) => {
+  // Accessible by Admin users only
+  // Set new user's auth level
+  res.send('Register page')
+});
+
+app.get('/edit_user', (req, res, next) => {
+  // Accessible by Admin users only
+  // edit user's auth level
+  res.send('Edit User page')
+});
+
+app.get('/add_new_exclusion', (req, res, next) => {
+  // Accessible by Admin and supervisors only
+  res.send('Add New Exclusion Page')
+});
+
+app.get('/edit_exclusion', (req, res, next) => {
+  // Accessible by Admin and supervisors only
+  res.send('Edit Exclusion Page')
+});
+
+app.get('/archive_exclusion', (req, res, next) => {
+  // Accessible by Admin and supervisors only
+  res.send('Archive Exclusion Page')
+});
+
+app.get('/past_orders', (req, res, next) => {
+  res.send('Past Exclusion Orders Page')
+});
+
+
+//*********** POST ROUTES ************/
 // Login POST route
 app.post('/login', (req, res, next) => {
   // Uses passport.js to authenticate user
 });
+
+// Register POST route
+app.post('/register', (req, res, next) => {
+  // Uses passport.js to register a new user, set their auth level
+});
+
+app.post('/edit_user', (req, res) => {
+  // Edit a registered user from /edit_user GET route
+});
+
+// add_exclusion POST route
+app.post('/add_exclusion', (req, res, next) => {
+  // Add a new exclusion from /add_new_exclusion GET route
+});
+
+// edit_exclusion POST route
+app.post('/edit_exclusion', (req, res, next) => {
+  // Add a new exclusion from /edit_exclusion GET route
+});
+
+// archive_exclusion POST route
+app.post('/archive_exclusion', (req, res, next) => {
+  // Archive exclusion, from archive_exclusion GET route
+});
+
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
