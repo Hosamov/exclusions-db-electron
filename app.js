792d127361549c590453a145484391d07eb0ecc3
Hosamov <hosamov@hotmail.com>
2022-11-30 10:45:48 -0800

    add more packages, additional routes

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const express = require('express');
 
+// Initialize DB:
+require('./initDB')();
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('HELLO WORLD');
+// Connect to local db:
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Home GET route - redirects to login or homepage depending on authorization
+app.get('/', (req, res, next) => {
+  res.send('Root page');
+});
+
+app.get('/home', (req, res, next) => {
+  res.send('Home page');
+});
+
+app.get('/home', (req, res, next) => {
+  res.send('Home page');
+});
+
+// Login POST route
+app.post('/login', (req, res, next) => {
+  // Uses passport.js to authenticate user
 });
 
 app.listen(3000, () => {
