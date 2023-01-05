ae28a4ef2d072de0e3000208363c614e46d75cc2
Hosamov <hosamov@hotmail.com>
2023-01-05 14:19:41 -0800

    update menus on each page to match. Update /errors route

--- app.js
+++ app.js
@@ -86,8 +86,8 @@ app.use((err, req, res, next) => {
     } else {
       err.message = err.message; //|| "Oops, it looks like something went wrong on the server...";
       res.status(err.status || 500).render('./errors/error', { err }); //display the error status and render the error template w/ error message/object
-      console.log(err.status);
-      console.log(err.message);
+      console.log('Error status code: ' + err.status);
+  console.log(err.stack);
     }
   }
 });
